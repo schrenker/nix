425a5e6865697f96297f493e16e9095267e15a70
schrenker <zawadzkis95@gmail.com>
2021-12-27 23:17:18 +0100

    font changes + cider changes

--- config.el
+++ config.el
@@ -33,8 +33,7 @@
  user-mail-address (rot13 "mnjnqmxvf95@tznvy.pbz")
 
  doom-theme 'doom-one
- doom-font (font-spec :family "JetBrains Mono NL" :size 13 )
- doom-big-font (font-spec :family "JetBrains Mono NL" :size 16)
+ doom-font (font-spec :family "JetBrains Mono NL" :size 12 )
  doom-themes-treemacs-theme "doom-colors"
  display-line-numbers-type 'relative
 
@@ -60,7 +59,7 @@
  auth-sources '("~/.authinfo.gpg")
  auth-source-cache-expiry nil
 
- org-directory "/Users/sebastian/Code/engineer_notebook"
+ org-directory "/Users/sebastian/code/engineer_notebook"
  org-babel-python-command "python3"
  org-superstar-headline-bullets-list '("⁖")
  org-superstar-prettify-item-bullets nil
@@ -76,12 +75,37 @@
  company-idle-delay 0
  company-require-match nil
 
+ cider-repl-pop-to-buffer-on-connect nil
+ cider-repl-display-in-current-window nil
+ cider-repl-display-help-banner nil
+ cider-repl-use-pretty-printing t
+
+ TeX-engine-alist
+   '((xetex "XeTeX -shell escape"
+            "xetex -shell-escape"
+            "xelatex -shell-escape"))
+
  scroll-margin 5)
+
+(defun adjust-org-company-backends ()
+  (remove-hook 'after-change-major-mode-hook '+company-init-backends-h)
+  (setq-local company-backends nil))
+
+(defun my/apply-theme (appearance)
+  "Load theme, taking current system APPEARANCE into consideration."
+  (mapc #'disable-theme custom-enabled-themes)
+  (pcase appearance
+    ('light (setq doom-theme 'doom-one-light)
+             (load-theme 'doom-one-light t))
+    ('dark (setq doom-theme 'doom-one)
+             (load-theme 'doom-one t))))
 
 (add-hook! org-mode
                 (adjust-org-company-backends)
                 (electric-indent-local-mode -1))
 (add-hook 'org-mode-hook(lambda () ( company-mode -1)))
+
+(add-hook 'ns-system-appearance-change-functions #'my/apply-theme)
 
 (after! lsp-clients
         (lsp-register-client
@@ -99,7 +123,16 @@
      ("[INPROGRESS]" :foreground "#98BE65" :weight normal)
      ("[WAITING]" :foreground "#DA8548" :weight normal)
      ("[DONE]" :foreground "#9FA4BB" :weight normal)
-     ("[CANCELLED]" :foreground "#574C58" :weight normal))))
+     ("[CANCELLED]" :foreground "#574C58" :weight normal)))
+  (custom-set-faces!
+    '(org-level-1 :height 1.04 :inherit outline-1)
+    '(org-level-2 :height 1.04 :inherit outline-2)
+    '(org-level-3 :height 1.03 :inherit outline-3)
+    '(org-level-4 :height 1.03 :inherit outline-4)
+    '(org-level-5 :height 1.02 :inherit outline-5)
+    '(org-level-6 :height 1.02 :inherit outline-6)
+    '(org-level-7 :height 1.01 :inherit outline-7)
+    '(org-level-8 :height 1.01 :inherit outline-8)))
 
 (after! org-fancy-priorities
   (setq
@@ -120,33 +153,6 @@
   (map-delete sp-pairs 'tex-mode)
   (map-delete sp-pairs 'plain-tex-mode))
 
-(defun adjust-org-company-backends ()
-  (remove-hook 'after-change-major-mode-hook '+company-init-backends-h)
-  (setq-local company-backends nil))
-
-;; (defun synchronize-theme ()
-;;   (let* ((light-theme 'doom-one-light)
-;;          (dark-theme 'doom-one)
-;;          (start-time-light-theme 6)
-;;          (end-time-light-theme 15)
-;;          (hour (string-to-number (substring (current-time-string) 11 13)))
-;;          (next-theme (if (member hour (number-sequence start-time-light-theme end-time-light-theme))
-;;                          light-theme dark-theme)))
-;;     (when (not (equal doom-theme next-theme))
-;;       (setq doom-theme next-theme)
-;;       (load-theme next-theme))))
-;; (run-with-timer 0 900 'synchronize-theme)
 
 (projectile-discover-projects-in-search-path)
 (projectile-cleanup-known-projects)
-
-(defun my/apply-theme (appearance)
-  "Load theme, taking current system APPEARANCE into consideration."
-  (mapc #'disable-theme custom-enabled-themes)
-  (pcase appearance
-    ('light (setq doom-theme 'doom-one-light)
-             (load-theme 'doom-one-light t))
-    ('dark (setq doom-theme 'doom-one)
-             (load-theme 'doom-one t))))
-
-(add-hook 'ns-system-appearance-change-functions #'my/apply-theme)

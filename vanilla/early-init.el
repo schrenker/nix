fd5f6be166c2aa542b68a448677599c7c3a44c0a
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-03-13 22:08:05 +0100

    moved the comp driver options to early-init.el

--- vanilla/early-init.el
+++ vanilla/early-init.el
@@ -2,5 +2,7 @@
 
 (setq package-enable-at-startup nil)
 
+(when (eq system-type 'darwin) (customize-set-variable 'native-comp-driver-options '("-Wl,-w")))
+
 ;; Fix for ld: library not found for -lemutls_w libgccjit.so: error: error invoking gcc driver
 (setenv "LIBRARY_PATH" "/opt/homebrew/opt/gcc/lib/gcc/12:/opt/homebrew/opt/libgccjit/lib/gcc/12:/opt/homebrew/opt/gcc/lib/gcc/12/gcc/aarch64-apple-darwin21/12")

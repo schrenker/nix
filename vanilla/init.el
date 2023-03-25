efc967613630dbfac57d1807362da468c13e567c
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-03-25 10:12:07 +0100

    set consult project function (project.el)

--- vanilla/init.el
+++ vanilla/init.el
@@ -363,9 +363,6 @@
   :config
   (inheritenv-add-advice #'with-temp-buffer))
 
-
-
-
 (use-package embark
   :bind
   (("M-." . embark-act)         ;; pick some comfortable binding
@@ -540,17 +537,7 @@ targets."
   ;; By default `consult-project-function' uses `project-root' from project.el.
   ;; Optionally configure a different project root function.
   ;;;; 1. project.el (the default)
-  ;; (setq consult-project-function #'consult--default-project--function)
-  ;;;; 2. vc.el (vc-root-dir)
-  ;; (setq consult-project-function (lambda (_) (vc-root-dir)))
-  ;;;; 3. locate-dominating-file
-  ;; (setq consult-project-function (lambda (_) (locate-dominating-file "." ".git")))
-  ;;;; 4. projectile.el (projectile-project-root)
-  ;; (autoload 'projectile-project-root "projectile")
-  ;; (setq consult-project-function (lambda (_) (projectile-project-root)))
-  ;;;; 5. No project support
-  ;; (setq consult-project-function nil)
-  )
+  (setq consult-project-function   #'consult--default-project-function))
 
 (use-package posframe)
 

439ed2fc39bbfa9859209f608d7ac7ad6681f2ba
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-03-25 10:11:33 +0100

    setenv LIBRARY_PATH only for macOS

--- vanilla/early-init.el
+++ vanilla/early-init.el
@@ -1,10 +1,10 @@
 ;;; vanilla/early-init.el -*- lexical-binding: t; -*-
 
-
 (setq package-enable-at-startup nil
       user-emacs-directory "~/.cache/emacs")
 
-(when (eq system-type 'darwin) (customize-set-variable 'native-comp-driver-options '("-Wl,-w")))
-
 ;; Fix for ld: library not found for -lemutls_w libgccjit.so: error: error invoking gcc driver
-(setenv "LIBRARY_PATH" "/opt/homebrew/opt/gcc/lib/gcc/12:/opt/homebrew/opt/libgccjit/lib/gcc/12:/opt/homebrew/opt/gcc/lib/gcc/12/gcc/aarch64-apple-darwin21/12")
+(when (eq system-type 'darwin)
+  (customize-set-variable 'native-comp-driver-options '("-Wl,-w"))
+  (setenv "LIBRARY_PATH" "/opt/homebrew/opt/gcc/lib/gcc/12:/opt/homebrew/opt/libgccjit/lib/gcc/12:/opt/homebrew/opt/gcc/lib/gcc/12/gcc/aarch64-apple-darwin21/12"))
+

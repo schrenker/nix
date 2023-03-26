f22e023e40b58c8f063993d107c866c77cf47f92
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-03-26 14:47:43 +0200

    completion styles now include substring, and go from least to most flexible

--- vanilla/init.el
+++ vanilla/init.el
@@ -291,7 +291,7 @@
   ;; Configure a custom style dispatcher (see the Consult wiki)
   ;; (setq orderless-style-dispatchers '(+orderless-consult-dispatch orderless-affix-dispatch)
   ;;       orderless-component-separator #'orderless-escapable-split-on-space)
-  (setq completion-styles '(orderless basic)
+  (setq completion-styles '(basic substring orderless)
         completion-category-defaults nil
         completion-category-overrides '((file (styles basic partial-completion)))
         orderless-matching-styles '(orderless-literal

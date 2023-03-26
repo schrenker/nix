1d87a062734144b3c3566e56ce90d676e250ddbe
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-03-26 14:41:36 +0200

    use consult for complete in region if vertico is enabled

--- vanilla/init.el
+++ vanilla/init.el
@@ -568,6 +568,14 @@ targets."
   ;; Configure other variables and modes in the :config section,
   ;; after lazily loading the package.
   :config
+  ;; Use `consult-completion-in-region' if Vertico is enabled.
+  ;; Otherwise use the default `completion--in-region' function.
+  (setq completion-in-region-function
+        (lambda (&rest args)
+          (apply (if vertico-mode
+                     #'consult-completion-in-region
+                   #'completion--in-region)
+                 args)))
 
   ;; Optionally configure preview. The default value
   ;; is 'any, such that any key triggers the preview.

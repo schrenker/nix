54661844c51c88af5550459cf3dd5f18c5a94b05
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-03-28 17:40:16 +0200

    disabled default completion ui for M-: and M-!

--- vanilla/init.el
+++ vanilla/init.el
@@ -571,7 +571,7 @@
 
   ;; Enable automatic preview at point in the *Completions* buffer. This is
   ;; relevant when you use the default completion UI.
-  :hook (completion-list-mode . consult-preview-at-point-mode)
+;  :hook (completion-list-mode . consult-preview-at-point-mode)
 
   ;; The :init configuration is always executed (Not lazy)
   :init

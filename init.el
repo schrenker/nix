e1e83ecc5ce55f500df39a4f962d92d31c6163db
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-04-16 12:51:14 +0200

    initial window control hydra

--- init.el
+++ init.el
@@ -342,11 +342,22 @@
                                 (?. . sentence))))
 
 (use-package hydra
+  :after ace-window
   :config
-  (defhydra hydra-window (global-map "C-c w")
+  (defhydra hydra-window (global-map "M-o")
     "Window"
-    ("u" winner-undo "undo")
-    ("r" winner-redo "redo"))
+    ("=" balance-windows "Balance windows")
+    ("2" schrenker/split-and-follow-vertically "Split down")
+    ("3" schrenker/split-and-follow-horizontally "Split right")
+    ("P" windmove-up "Move windown up")
+    ("N" windmove-down "Move window down")
+    ("H" windmove-left "Move window left")
+    ("T" windmove-right "Move window right")
+    ("m" maximize-window "Maximize window")
+    ("o" aw-flip-window "Switch window")
+    ("u" winner-undo "Winner undo")
+    ("r" winner-redo "Winner redo")
+    ("M-o" ace-window "ace-window"))
   (defhydra hydra-text-scale (global-map "<f2>")
     "Text Zoom"
     ("=" text-scale-increase "Zoom In")
@@ -775,22 +786,23 @@ targets."
 (use-package posframe)
 
 (use-package ace-window
-  :bind ("M-o" . ace-window)
+  ;:bind ("M-o" . ace-window)
   :config
   (require 'posframe)
   (setq aw-keys '(?e ?t ?u ?h ?o ?n ?a ?s)
-        aw-dispatch-alist'((?k aw-delete-window "Delete Window")
-                           (?m aw-swap-window "Swap Windows")
-                           (?M aw-move-window "Move Window")
-                           (?x aw-copy-window "Copy Window")
-                           (?b aw-switch-buffer-in-window "Select Buffer")
-                           (?\M-o aw-flip-window)
-                           (?B aw-switch-buffer-other-window "Switch Buffer Other Window")
-                           (?w aw-split-window-fair "Split Fair Window")
-                           (?v aw-split-window-vert "Split Vert Window")
-                           (?z aw-split-window-horz "Split Horz Window")
-                           (?K delete-other-windows "Delete Other Windows")
-                           (?? aw-show-dispatch-help)))
+        ;; aw-dispatch-alist';; ((?k aw-delete-window "Delete Window")
+                          ;;  (?m aw-swap-window "Swap Windows")
+                          ;;  (?M aw-move-window "Move Window")
+                          ;;  (?x aw-copy-window "Copy Window")
+                          ;;  (?b aw-switch-buffer-in-window "Select Buffer")
+                          ;;  (?\M-o aw-flip-window)
+                          ;;  (?B aw-switch-buffer-other-window "Switch Buffer Other Window")
+                          ;;  (?w aw-split-window-fair "Split Fair Window")
+                          ;;  (?v aw-split-window-vert "Split Vert Window")
+                          ;;  (?z aw-split-window-horz "Split Horz Window")
+                          ;;  (?K delete-other-windows "Delete Other Windows")
+                          ;;  (?? aw-show-dispatch-help))
+        )
   (ace-window-posframe-mode 1))
 
 ;; (use-package popwin

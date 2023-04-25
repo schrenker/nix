6db04391fa16bb314a561c3cfb1cb445e547b7c6
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-04-25 15:58:29 +0200

    guard against org roam not being available

--- init.el
+++ init.el
@@ -1037,7 +1037,9 @@ ARCHIVE_CATEGORY, ARCHIVE_TODO, and ARCHIVE_ITAGS properties."
       (org-refile nil nil (list nil orig-file nil (org-find-olp `(,orig-file ,@(split-string orig-path "/")) nil)))))
 
   (add-hook 'org-mode-hook (lambda () (visual-line-mode 1)))
-  (add-hook 'org-mode-hook (lambda () (unless (org-roam-capture-p)) #'org-format-on-save-mode))
+  (add-hook 'org-mode-hook (lambda () (unless
+                                     (and (fboundp 'org-roam-capture-p) (org-roam-capture-p))
+                                   #'org-format-on-save-mode)))
   (add-hook 'org-mode-hook (lambda () (electric-indent-local-mode -1)))
   (add-hook 'org-mode-hook
             (lambda ()

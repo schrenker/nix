6440b93fd26d83b37dec2747f02827e03a55ecc3
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-06-13 00:46:14 +0200

    seems like :after block wasn't allowing eglot bind to run

--- init.el
+++ init.el
@@ -1394,19 +1394,17 @@ ARCHIVE_CATEGORY, ARCHIVE_TODO, and ARCHIVE_ITAGS properties."
   (setq multi-vterm-dedicated-window-height-percent 30))
 
 (use-package eglot
-  :after cape
-  :demand t
-  :elpaca nil
-  :config
-  (bind-keys :map eglot-mode-map
-             ("C-c c c" . eglot)
-             ("C-c c f" . eglot-format)
-             ("C-c c a" . eglot-code-actions)
-             ("C-c c r" . eglot-rename)
-             ("C-c c i" . eglot-find-implementation)
-             ("C-c c d" . eglot-find-declaration)
-             ("C-c c t" . eglot-find-typeDefinition))
+  :bind
+  (:map eglot-mode-map
+        ("C-c c c" . eglot)
+        ("C-c c f" . eglot-format)
+        ("C-c c a" . eglot-code-actions)
+        ("C-c c r" . eglot-rename)
+        ("C-c c i" . eglot-find-implementation)
+        ("C-c c d" . eglot-find-declaration)
+        ("C-c c t" . eglot-find-typeDefinition))
 
+  :config
   (defun schrenker/eglot-capf ()
     (setq-local completion-at-point-functions
                 (list (cape-super-capf

dd36af98620d3b0c53421104e2839b5da46c922d
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-07-03 22:54:53 +0200

    new LIBRARY_PATH after move from homebrew to nix

--- early-init.el
+++ early-init.el
@@ -10,7 +10,8 @@
 ;; Fix for ld: library not found for -lemutls_w libgccjit.so: error: error invoking gcc driver
 (when (eq system-type 'darwin)
   (customize-set-variable 'native-comp-driver-options '("-Wl,-w"))
-  (setenv "LIBRARY_PATH" "/opt/homebrew/opt/gcc/lib/gcc/13:/opt/homebrew/opt/libgccjit/lib/gcc/13:/opt/homebrew/opt/gcc/lib/gcc/13/gcc/aarch64-apple-darwin21/13"))
+  ;; (setenv "LIBRARY_PATH" "/opt/homebrew/opt/gcc/lib/gcc/13:/opt/homebrew/opt/libgccjit/lib/gcc/13:/opt/homebrew/opt/gcc/lib/gcc/13/gcc/aarch64-apple-darwin21/13")
+  (setenv "LIBRARY_PATH" "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"))
 
 (provide 'early-init)
 ;;; early-init.el ends here.

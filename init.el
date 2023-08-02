df1cbc2f00925f6eb93ff4b9089be8eccbaae502
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-08-02 19:20:23 +0200

    cleanup and cache buster for eglot

--- init.el
+++ init.el
@@ -437,36 +437,14 @@ frame if FRAME is nil, and to 1 if AMT is nil."
   (corfu-popupinfo-mode 1))
 
 (use-package cape
-  :after corfu
-  ;; Bind dedicated completion commands
-  ;; Alternative prefix keys: C-c p, M-p, M-+, ...
-  :commands
-  (completion-at-point ;; capf
-   complete-tag       ;; etags
-   cape-dabbrev       ;; or dabbrev-completion
-   cape-history
-   cape-file
-   cape-keyword
-   cape-symbol
-   cape-abbrev
-   cape-ispell
-   cape-dict
-   cape-sgml
-   cape-rfc1345)
+  :after (eglot corfu)
   :init
   ;; Add `completion-at-point-functions', used by `completion-at-point'.
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
-  ;; (add-to-list 'completion-at-point-functions #'cape-keyword)
-  ;; (add-to-list 'completion-at-point-functions #'cape-dict)
   (add-to-list 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-elisp-block)
-  ;; (add-to-list 'completion-at-point-functions #'cape-history)
-  ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
-  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
-  ;; (add-to-list 'completion-at-point-functions #'cape-abbrev)
-  ;; (add-to-list 'completion-at-point-functions #'cape-ispell)
-  ;; (add-to-list 'completion-at-point-functions #'cape-symbol)
-  )
+  :config
+  (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster))
 
 (use-package kind-icon
   :commands (kind-icon-margin-formatter kind-icon-reset-cache)

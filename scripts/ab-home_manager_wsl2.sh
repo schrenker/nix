91ced1906ad6dffbcb8c9bcf288e8e4ef82dfc07
Sebastian Zawadzki <sebastian@zawadzki.tech>
2023-11-08 10:14:00 +0100

    switch shell

--- scripts/ab-home_manager_wsl2.sh
+++ scripts/ab-home_manager_wsl2.sh
@@ -1,10 +1,12 @@
-echo '/home/sebastian/.nix-profile/bin/fish' | sudo tee -a /etc/shells
-
 cd ~/.config/nix
 
 nix build .#homeConfigurations.WSL2
 
 ./result/activate switch --flake ~/.config/nix#WSL2
+
+echo '/home/sebastian/.nix-profile/bin/fish' | sudo tee -a /etc/shells
+
+chsh -s /home/sebastian/.nix-profile/bin/fish
 
 home-manager switch --flake ~/.config/nix#WSL2
 

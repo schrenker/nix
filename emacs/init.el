bc0c10d153723903ccb70c1800bdfe28bdedc11e
Sebastian Zawadzki <sebastian@zawadzki.tech>
2024-03-17 15:49:18 +0100

    elpaca update

--- emacs/init.el
+++ emacs/init.el
@@ -159,12 +159,12 @@
                  (cancel-timer timer)
                (setq counter (1+ counter))))))))
 
-(defvar elpaca-installer-version 0.6)
+(defvar elpaca-installer-version 0.7)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil
+                              :ref nil :depth 1
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
@@ -177,8 +177,10 @@
     (when (< emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
         (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                 ((zerop (call-process "git" nil buffer t "clone"
-                                       (plist-get order :repo) repo)))
+                 ((zerop (apply #'call-process `("git" nil ,buffer t "clone"
+                                                 ,@(when-let ((depth (plist-get order :depth)))
+                                                     (list (format "--depth=%d" depth) "--no-single-branch"))
+                                                 ,(plist-get order :repo) ,repo))))
                  ((zerop (call-process "git" nil buffer t "checkout"
                                        (or (plist-get order :ref) "--"))))
                  (emacs (concat invocation-directory invocation-name))
@@ -220,7 +222,7 @@
 
 (use-package envrc
   :if (or (executable-find "direnv") (executable-find "nix") (eq system-type 'darwin))
-  :hook (after-init . envrc-global-mode)
+  :hook (elpaca-after-init . envrc-global-mode)
   :init
   (defvar envrc-after-update-hook nil)
   (advice-add #'envrc--update :after (lambda () (run-hooks 'envrc-after-update-hook))))

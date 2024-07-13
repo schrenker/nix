ff69109f20970a32cf233c35f8d2c0a77fa82096
Sebastian Zawadzki <sebastian@zawadzki.tech>
2024-07-13 14:18:22 +0200

    treesit package so I can download grammars

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -1781,6 +1781,34 @@ Purpose of this is to be able to go back to Dired window with aw-flip-window, if
             (t ,(nerd-icons-codicon "nf-cod-code") :face font-lock-warning-face)))
   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
+(use-package treesit
+  :ensure nil
+  :init
+  (setopt treesit-language-source-alist
+          '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+            (c "https://github.com/tree-sitter/tree-sitter-c")
+            (cmake "https://github.com/uyha/tree-sitter-cmake")
+            (common-lisp "https://github.com/theHamsta/tree-sitter-commonlisp")
+            (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+            (css "https://github.com/tree-sitter/tree-sitter-css")
+            (csharp "https://github.com/tree-sitter/tree-sitter-c-sharp")
+            (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+            (go "https://github.com/tree-sitter/tree-sitter-go")
+            (go-mod "https://github.com/camdencheek/tree-sitter-go-mod")
+            (html "https://github.com/tree-sitter/tree-sitter-html")
+            (js . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
+            (json "https://github.com/tree-sitter/tree-sitter-json")
+            (lua "https://github.com/Azganoth/tree-sitter-lua")
+            (make "https://github.com/alemuller/tree-sitter-make")
+            (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+            (python "https://github.com/tree-sitter/tree-sitter-python")
+            (r "https://github.com/r-lib/tree-sitter-r")
+            (rust "https://github.com/tree-sitter/tree-sitter-rust")
+            (toml "https://github.com/tree-sitter/tree-sitter-toml")
+            (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
+            (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
+            (typst . ("https://github.com/uben0/tree-sitter-typst" "master" "src")))))
+
 (use-package ligature
   :config
   ;; Enable all JetBrains Mono ligatures in programming modes

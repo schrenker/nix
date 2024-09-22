36a9a3234c310a867aeb6b71283d196d9f8654bc
Sebastian Zawadzki <sebastian@zawadzki.tech>
2024-09-22 17:15:28 +0200

    add second devShell for linux

--- flake.nix
+++ flake.nix
@@ -103,5 +103,12 @@
         in pkgs.mkShell {
           packages = with pkgs; [ inputs.nil.packages.${system}.nil ];
         };
+
+      devShells.x86_64-linux.default =
+        let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in pkgs.mkShell {
+          packages = with pkgs; [ inputs.nil.packages.${system}.nil ];
+        };
+
     };
 }

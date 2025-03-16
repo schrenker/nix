aea8e473fef5801ab2a882ef213783c2c368284d
Sebastian Zawadzki <sebastian@zawadzki.tech>
2025-03-16 23:36:36 +0100

    remove grammar links as I source them from nix now

--- hosts/common/dotfiles/emacs/init.el
+++ hosts/common/dotfiles/emacs/init.el
@@ -1910,34 +1910,7 @@ Additionally, disable dired-preview-mode, if target buffer is dired buffer."
   (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
 
 (use-package treesit
-  :ensure nil
-  ;; :init
-  ;; (setopt treesit-language-source-alist
-  ;;         '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-  ;;           (c "https://github.com/tree-sitter/tree-sitter-c")
-  ;;           (cmake "https://github.com/uyha/tree-sitter-cmake")
-  ;;           (common-lisp "https://github.com/theHamsta/tree-sitter-commonlisp")
-  ;;           (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
-  ;;           (css "https://github.com/tree-sitter/tree-sitter-css")
-  ;;           (csharp "https://github.com/tree-sitter/tree-sitter-c-sharp")
-  ;;           (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-  ;;           (go "https://github.com/tree-sitter/tree-sitter-go")
-  ;;           (go-mod "https://github.com/camdencheek/tree-sitter-go-mod")
-  ;;           (html "https://github.com/tree-sitter/tree-sitter-html")
-  ;;           (js . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
-  ;;           (json "https://github.com/tree-sitter/tree-sitter-json")
-  ;;           (lua "https://github.com/Azganoth/tree-sitter-lua")
-  ;;           (make "https://github.com/alemuller/tree-sitter-make")
-  ;;           (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-  ;;           (python "https://github.com/tree-sitter/tree-sitter-python")
-  ;;           (r "https://github.com/r-lib/tree-sitter-r")
-  ;;           (rust "https://github.com/tree-sitter/tree-sitter-rust")
-  ;;           (toml "https://github.com/tree-sitter/tree-sitter-toml")
-  ;;           (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
-  ;;           (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
-  ;;           (typst . ("https://github.com/uben0/tree-sitter-typst" "master" "src"))
-  ;;           (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "master" "src"))))
-  )
+  :ensure nil)
 
 (use-package ligature
   :config

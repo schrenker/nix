87b56af21be297f89f8650a32bb19f6129f5aa8f
Sebastian Zawadzki <sebastian@zawadzki.tech>
2025-04-17 21:46:21 +0200

    attempt to fix weird coding upon paste in wsl2

--- hosts/common/dotfiles/emacs/init.el
+++ hosts/common/dotfiles/emacs/init.el
@@ -39,6 +39,7 @@
 
   (when (schrenker/wsl2-p)
     (setopt emacs-appearance 'light
+            selection-coding-system nil
             browse-url-generic-program  "/mnt/c/Windows/System32/cmd.exe"
             browse-url-generic-args     '("/c" "start")
             browse-url-browser-function #'browse-url-generic))

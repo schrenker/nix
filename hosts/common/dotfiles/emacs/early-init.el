0393f0bcee41ada1c2c9083fd11e238abd1f6612
Sebastian Zawadzki <sebastian@zawadzki.tech>
2025-12-08 23:13:08 +0100

    comment out native comp section in early-init.el

--- hosts/common/dotfiles/emacs/early-init.el
+++ hosts/common/dotfiles/emacs/early-init.el
@@ -8,20 +8,20 @@
 (setq package-enable-at-startup nil)
 
 ;; Fix for ld: library not found for -lemutls_w libgccjit.so: error: error invoking gcc driver
-(when (eq system-type 'darwin)
-  (customize-set-variable 'native-comp-driver-options '("-Wl,-w"))
-  (setq native-comp-async-jobs-number 8)
-  ;; (setenv "LIBRARY_PATH" "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib")
-  ;; (setenv "LIBRARY_PATH"
-  ;;         (let*
-  ;;             ((base "/opt/homebrew/opt/gcc/lib/gcc/")
-  ;;              (version (car (directory-files base nil "[^a-zA-Z.]" nil 1)))
-  ;;              (macversion (car (directory-files (concat base version "/gcc/") nil "[^.]" nil 1)))
-  ;;              (gcc (concat base version))
-  ;;              (libgccjit (concat "/opt/homebrew/opt/libgccjit/lib/gcc/" version))
-  ;;              (macgcc (concat base version "/gcc/" macversion "/" version)))
-  ;;           (string-join (list gcc libgccjit macgcc) ":")))
-  )
+;; (when (eq system-type 'darwin)
+;;   (customize-set-variable 'native-comp-driver-options '("-Wl,-w"))
+;;   (setq native-comp-async-jobs-number 8)
+;;   ;; (setenv "LIBRARY_PATH" "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib")
+;;   ;; (setenv "LIBRARY_PATH"
+;;   ;;         (let*
+;;   ;;             ((base "/opt/homebrew/opt/gcc/lib/gcc/")
+;;   ;;              (version (car (directory-files base nil "[^a-zA-Z.]" nil 1)))
+;;   ;;              (macversion (car (directory-files (concat base version "/gcc/") nil "[^.]" nil 1)))
+;;   ;;              (gcc (concat base version))
+;;   ;;              (libgccjit (concat "/opt/homebrew/opt/libgccjit/lib/gcc/" version))
+;;   ;;              (macgcc (concat base version "/gcc/" macversion "/" version)))
+;;   ;;           (string-join (list gcc libgccjit macgcc) ":")))
+;;   )
 
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
